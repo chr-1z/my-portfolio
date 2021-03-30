c162d16917678f215a075cb7955665326e78666b
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-03-30 17:34:02 -0300

    until here

--- script.js
+++ script.js
@@ -2,21 +2,28 @@ document.querySelector(".hamburguer").addEventListener("click", function() {
     document.querySelector(".container").classList.toggle("show-menu");
 });
 
-mybutton = document.getElementById("myBtn");
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var rootElement = document.documentElement;
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.80) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn");
   } else {
-    mybutton.style.display = "none";
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn");
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 }
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+document.addEventListener("scroll", handleScroll);
