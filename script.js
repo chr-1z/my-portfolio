8e696c90619d956fd3dda8648a7e0f765292e5f3
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-12-28 09:17:16 -0300

    Ajuste scrolltop

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var rootElement = document.documentElement;
 function handleScroll() {
   // Do something on scroll
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-  if ((rootElement.scrollTop / scrollTotal ) > 0.80) {
+  if ((rootElement.scrollTop / scrollTotal ) > 0.97) {
     // Show button
     scrollToTopBtn.classList.add("showBtn");
   } else {
