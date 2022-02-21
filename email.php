ce951347d3834c2651d1fbe9b455eaf293b03064
Christian <chris99leichtweiss@gmail.com>
2022-02-20 21:12:58 -0300

    update: email.php

--- email.php
+++ email.php
@@ -4,23 +4,26 @@ $nome = addcslashes($_POST['name']);
 $email = addcslashes($_POST['email']);
 $mensagem =  addcslashes($_POST['message']);
 
-function enviaEmail() {
+function enviaEmail($nome, $email, $mensagem) {
 
-if(isset($_POST['email']) && !empty($_POST['email'])){
+    if(isset($_POST['email']) && !empty($_POST['email'])){
 
-    $to = $email;
-    $subject = "Contato - Formulário";
-    $body = "Nome: ".$nome. "\n"
-            "Mensagem: ".$mensagem;
-    $header = "From:chris99leichtweiss@gmail.com"."\r\n". 
-                "Reply-To:".$email."\r\n". 
-                "X=Mailer:PHP/".phpversion();
-    
-    if(mail($to, $subject, $body, $header)) {
-        echo("Email enviado com sucesso!");
-    } else {
-        echo("O email não pode ser enviado!");
+        $to = $email;
+        $subject = "Contato - Formulário";
+        $body = "Nome: ".$nome. "\n"
+                "Mensagem: ".$mensagem;
+        $header = "From:chris99leichtweiss@gmail.com"."\r\n". 
+                    "Reply-To:".$email."\r\n". 
+                    "X=Mailer:PHP/".phpversion();
+        
+        if(mail($to, $subject, $body, $header)) {
+            echo("Email enviado com sucesso!");
+        } else {
+            echo("O email não pode ser enviado!");
+        }
     }
 }
 
+enviaEmail($nome, $email, $mensagem);
+    
 ?>
